789556d737eedc70cb855bd29fe5cc944b2e1aaf
Thiago Rodrigues <thiagoney@outlook.com>
2022-01-07 16:18:10 -0300

    Renamed files name (day 1)

--- 01-Beginner/code01_10.py
+++ 01-Beginner/code01_10.py
@@ -1,4 +1,2 @@
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the \"+\" sign.")
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+name = input("What is your name?")
+print(len(name))
