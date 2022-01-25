b25e3aa8b6c63801d96bcaf69745d360a9e6252e
Thiago Rodrigues <thiagoney@outlook.com>
2022-01-25 00:24:24 -0300

    Upload folder project12

---
+++ 01-Beginner/projects/project12/main.py
@@ -0,0 +1,65 @@
+"""
+Number Guessing Game!
+"""
+
+# Imports
+
+import os
+import random
+from logo import logo
+
+# Main
+
+data_options = {'easy': 10, 'hard': 5}
+
+restart_game = True
+while restart_game:
+    os.system('cls')
+
+    print(logo)
+    print('Welcome to the Number Guessing Game!')
+    print('I\'m thinking of a number between 1 and 100.')
+
+    option = input('Choose a difficulty. Type \'easy\' or \'hard\': ')
+    while option not in ['easy', 'hard']:
+        option = input('Invalid option! Please, try again: ')
+
+    number_attempt = data_options[option]
+    mistakes = 0
+    number = random.randint(1, 100)
+
+    continue_game = True
+    while continue_game:
+        print(f'\nYou have {number_attempt-mistakes} attempts remaining to guess the number.')
+
+        attempt = input('Make a guess: ')
+        while not attempt.isdigit():
+            attempt = input('Invalid option! Please, try again: ')
+
+        attempt = int(attempt)
+
+        if mistakes == number_attempt:
+            print(f'You\'ve run out of guesses, you lose. The answer was {number}')
+            continue_game = False
+        else:
+            if attempt < number:
+                print('\nToo low.')
+                continue_game = True
+                mistakes += 1
+            elif attempt > number:
+                print('\nToo high.')
+                continue_game = True
+                mistakes += 1
+            else:
+                print(f'\nYou got it! The answer was {number}.')
+                continue_game = False
+
+    play_again = input('\nDo you want play again? Type \'yes\' or \'no\': ')
+    while play_again not in ['yes', 'no']:
+        play_again = input('Invalid option! Please, try again: ')
+
+    if play_again == 'yes':
+        restart_game = True
+    else:
+        print('Bye bye!!!')
+        restart_game = False
