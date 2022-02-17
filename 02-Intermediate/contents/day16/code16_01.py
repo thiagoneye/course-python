9ad2df0a910ee71b2cdf9d76a12d495d2590878b
Thiago Rodrigues <thiagoney@outlook.com>
2022-02-16 21:28:17 -0300

    Initial commit (day 16)

---
+++ 02-Intermediate/contents/day16/code16_01.py
@@ -0,0 +1,14 @@
+"""
+100 Days of Code
+The Complete Python Pro Bootcamp
+
+Constructing Objects and Accessing their Attributes and Methods
+"""
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+my_screen = Screen()
+#print(my_screen.canvheight)
+my_screen.exitonclick()
