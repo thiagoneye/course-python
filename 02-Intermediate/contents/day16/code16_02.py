503059f6efc2ff679ff8002bda5b6a2311126d24
Thiago Rodrigues <thiagoney@outlook.com>
2022-02-16 22:32:11 -0300

    Initial commit (project 16)

--- 02-Intermediate/contents/day16/code16_02.py
+++ 02-Intermediate/contents/day16/code16_02.py
@@ -15,4 +15,6 @@ table = PrettyTable()
 table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander', 'Bulbasaur'])
 table.add_column('Type', ['Electric', 'Water', 'Fire', 'Grass'])
 
+table.align = 'l'
+
 print(table)
